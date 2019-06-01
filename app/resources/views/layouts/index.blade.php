9c0f099035792758d9f5ec60d7969e1bc4dff550
zloyleva <zloyleva@gmail.com>
2019-06-01 16:55:27 +0300

    midify Home blade

--- app/resources/views/layouts/index.blade.php
+++ app/resources/views/layouts/index.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title', 'Laravel course')</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    @yield('meta')
+
 </head>
 <body>
 
 
     @yield('content')
 
+    @section('javascript')
+    <script src="{{asset('js/app.js')}}"></script>
+    @show
 
 </body>
 </html>
--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -1,2 +1,24 @@
 @extends('layouts.index')
 
+@section('title')
+    Laravel course || Home page
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Home page</h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('javascript')
+@parent
+<script>
+
+    console.log('App was load...')
+
+</script>
+@endsection
